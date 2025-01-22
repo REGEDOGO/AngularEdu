432423f36b7086897db4b641f7e32a1d7c479602
Rege <85848521+REGEDOGO@users.noreply.github.com>
2025-01-22 12:08:15 +0100

    Podstawy komponentów

---
+++ Komponenty/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, MainComponent, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'CodeSandbox';
+}
---
+++ Komponenty/src/app/app.config.ts
@@ -0,0 +1,14 @@
+import {
+  ApplicationConfig,
+  NgModule,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+  ],
+};
